2d73a63873b113df101a981e8f032f2520159ca5
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-08-13 13:26:02 +0530

    Update socket.js

--- frontend/src/Utils/socket.js
+++ frontend/src/Utils/socket.js
@@ -1,9 +1,6 @@
 import { io } from "socket.io-client";
 
-const BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://localhost:4000"
-    : "http://192.168.1.19:4000";
+const BASE_URL ="https://whatsapp-web-r6dr.onrender.com"
 
 const socket = io(BASE_URL, {
   withCredentials: true,
