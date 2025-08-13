9e192dc7d9fda0a331703ec4bc826e803f4536e3
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-08-13 13:21:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ cloud.connectCloudinary();
 // Middlewares
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://192.168.1.19:5173"], // Your frontend
+    origin: ["https://whats-app-web-nu.vercel.app"], // Your frontend
     credentials: true,
 }));
 
@@ -50,7 +50,7 @@ app.get("/", (req, res) => {
 // Setup Socket.io
 const io = new Server(server, {
     cors: {
-        origin:[ "http://localhost:5173","http://192.168.1.19:5173"],
+        origin:[ "https://whats-app-web-nu.vercel.app"],
         credentials: true,
     },
 });
